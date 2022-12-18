52c9ef214933fcd82a6fe9ee8799266df0ef3d4f
Vaibhav <rawatvaibhav42@gmail.com>
2022-12-18 17:33:49 +0530

    edited the code

--- users.py
+++ users.py
@@ -52,7 +52,7 @@ def load_markov_chain(order):
         print("Type 3 to load 3rd order Markov Chain")
         sys.exit(0)
 
-#prediction
+#prediction for function
 def predict_next_word(state, chain, order):
     """
     Predicts the next word that will be typed.

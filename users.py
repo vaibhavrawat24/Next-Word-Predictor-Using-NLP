9d89473baf9f0bfee7c4521a6d198843778b1c12
Vaibhav <rawatvaibhav42@gmail.com>
2022-12-25 21:06:12 +0530

    edited code

--- users.py
+++ users.py
@@ -118,7 +118,7 @@ save = input("Do you wish to save your input? (y/n) ")
 if save in ['y', 'yes', 'YES', 'Y']:
     with open("user_input.txt","w") as f:
         f.write(' '.join(typed[:-1]))  
-
+#retrain
 retrain = input("Do you wish to retrain the chain with your input? (y/n) ")
 if retrain in ['y', 'yes', 'YES', 'Y']:
     try: 
